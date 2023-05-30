89c71e3febb5fb96f08e67c0b82909054a84e5f7
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2023-05-30 11:39:23 +0300

    add base

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { nanoid } from 'nanoid';
+import { Container } from 'components';
+// import { Container, Section, ContactForm, ContactList, Filter, Heading } from 'components';
+
+export class App extends React.Component {
+  render() {
+    return <Container></Container>;
+  }
+}
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export * from 'components/App/App';
+// export * from 'components/Section/Section.styled.jsx';
+export * from 'components/Container/Container.styled.jsx';
+// export * from 'components/Heading/Heading.styled.jsx';
